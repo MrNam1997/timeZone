325af92d8b3d22a2fd88b4a4e705a0b388576563
MrNam1997 <nammaco997@gmail.com>
2019-09-26 17:12:55 +0700

    check time zone city global

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/', function () {
+    return view('index');
+});
+Route::get('/{timezone?}', function ($timezone = null)
+{
+    if (!empty($timezone)) {
+        $time = new DateTime(date('y-m-d H:i:s'), new DateTimeZone( 'UTC'));
+        $time->setTimezone(new DateTimeZone(str_replace('-', '/', $timezone)));
+          echo 'Múi giờ bạn chọn ' . $timezone . ' hiện tại đang là: ' . $time->format('d-m-Y H:i:s');
+   } else {
+        return view('index');
+    }
+});
+
